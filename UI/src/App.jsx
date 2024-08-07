970d5899d65d5ae723238aaed579b00a6a70b269
Jhow Rodrigues <jhowrdev@gmail.com>
2024-08-07 19:14:48 -0300

    setup inicial do front

--- UI/src/App.jsx
+++ UI/src/App.jsx
@@ -1,19 +1,27 @@
-import { useState } from "react";
-
-import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
-import Footer from "./components/Footer";
-import { Box } from "@mui/system";
-
+import "./App.css";
+import Employee from "./pages/Employee";
+import { Home } from "./pages/Home";
+import { Sales } from "./pages/Sales";
+import { Commission } from "./pages/Commission";
 function App() {
   return (
-    <Box className="app">
-      <Header />
-      <Box className="content">
-        {/* Adicione aqui o conteúdo principal da sua aplicação */}
-      </Box>
-      <Footer />
-    </Box>
+    <Router>
+      <div className="app">
+        <Header />
+        <div className="content">
+          <Routes>
+            <Route path="/home" element={<Home />} />
+            <Route path="/funcionarios" element={<Employee />} />{" "}
+            {/* Rota para o componente Employee */}
+            <Route path="/vendas" element={<Sales />} />
+            <Route path="/comissoes" element={<Commission />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
--- UI/src/components/Header.jsx
+++ UI/src/components/Header.jsx
@@ -1,17 +1,40 @@
 import React from "react";
-import AppBar from "@mui/material/AppBar";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import TabContext from "@mui/lab/TabContext";
+import TabList from "@mui/lab/TabList";
+import TabPanel from "@mui/lab/TabPanel";
+import Tab from "@mui/material/Tab";
+import { Link } from "react-router-dom";
 
 const Header = () => {
+  const [value, setValue] = React.useState("1");
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   return (
-    <AppBar>
-      <Toolbar>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          Comissão dos Funcionários
-        </Typography>
-      </Toolbar>
-    </AppBar>
+    <Box className="header-container">
+      <TabContext value={value}>
+        <Box className="tab-container">
+          <TabList
+            onChange={handleChange}
+            aria-label="navigation tabs"
+            className="tab-list"
+          >
+            <Tab label="Home" value="1" component={Link} to="/home" />
+            <Tab
+              label="Funcionários"
+              value="2"
+              component={Link}
+              to="/funcionarios"
+            />
+            <Tab label="Vendas" value="3" component={Link} to="/vendas" />
+            <Tab label="Comissões" value="4" component={Link} to="/comissoes" />
+          </TabList>
+        </Box>
+      </TabContext>
+    </Box>
   );
 };
 

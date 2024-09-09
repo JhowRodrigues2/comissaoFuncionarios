ff413ec98b7d59227c97b0074497c20a0bec5ec9
Jhow Rodrigues <jhowrdev@gmail.com>
2024-09-09 20:54:53 -0300

    criação de função exportar para PDF

--- UI/src/pages/Commission.jsx
+++ UI/src/pages/Commission.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState, useEffect, useContext, useRef } from "react";
 import {
   Container,
   Grid,
@@ -17,6 +17,7 @@ import dayjs from "dayjs";
 import DateComponent from "../components/DatePicker";
 import { SelectList } from "../components/SelectList";
 import { EmployeeContext } from "../context/EmployeeContext";
+import { useReactToPrint } from "react-to-print";
 
 export const Commission = () => {
   const [selectedDate, setSelectedDate] = useState(dayjs());
@@ -38,6 +39,11 @@ export const Commission = () => {
     getSaleById(employeeId);
   };
 
+  const contentDocument = useRef();
+
+  const handlePrint = useReactToPrint({
+    content: () => contentDocument.current,
+  });
   return (
     <Container className="boxArea">
       <Typography variant="h4">
@@ -56,60 +62,69 @@ export const Commission = () => {
           <SelectList value={employeeId} onChange={setEmployeeId} />
         </Grid>
         <Grid item xs={12} sm={6} md={4}>
-          <Button variant="outlined" fullWidth onClick={filterEmployee}>
+          <Button
+            variant="outlined"
+            fullWidth
+            onClick={filterEmployee}
+            disabled={employeeId == ""}
+          >
             Aplicar Filtros
           </Button>
         </Grid>
       </Grid>
-
-      <Box mt={4}>
-        <Typography variant="h6" gutterBottom>
-          Funcionários
-        </Typography>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>Nome</TableCell>
-              <TableCell>Posição</TableCell>
-              <TableCell>E-mail</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {employeeSelected && (
-              <TableRow key={employeeSelected.id}>
-                <TableCell>{employeeSelected.name}</TableCell>
-                <TableCell>{employeeSelected.position}</TableCell>
-                <TableCell>{employeeSelected.email}</TableCell>
+      <Box ref={contentDocument}>
+        <Box mt={4}>
+          <Typography variant="h6" gutterBottom>
+            Funcionários
+          </Typography>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Nome</TableCell>
+                <TableCell>Posição</TableCell>
+                <TableCell>E-mail</TableCell>
               </TableRow>
-            )}
-          </TableBody>
-        </Table>
-      </Box>
-      <Box mt={4}>
-        <Typography variant="h6" gutterBottom>
-          Vendas
-        </Typography>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>Data</TableCell>
-              <TableCell>Forma de Pagamento</TableCell>
-              <TableCell>Valor</TableCell>
-              <TableCell>Comissão</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {saleEmployeeSelected.map((sale) => (
-              <TableRow key={sale.id}>
-                <TableCell>{dayjs(sale.date).format("DD/MM/YYYY")}</TableCell>
-                <TableCell>{paymentMethods[sale.paymentMethod]}</TableCell>
-                <TableCell>{formatCurrency(sale.value)}</TableCell>
-                <TableCell>{formatCurrency(sale.commission)}</TableCell>
+            </TableHead>
+            <TableBody>
+              {employeeSelected && (
+                <TableRow key={employeeSelected.id}>
+                  <TableCell>{employeeSelected.name}</TableCell>
+                  <TableCell>{employeeSelected.position}</TableCell>
+                  <TableCell>{employeeSelected.email}</TableCell>
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </Box>
+        <Box mt={4}>
+          <Typography variant="h6" gutterBottom>
+            Vendas
+          </Typography>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Data</TableCell>
+                <TableCell>Forma de Pagamento</TableCell>
+                <TableCell>Valor</TableCell>
+                <TableCell>Comissão</TableCell>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
+            </TableHead>
+            <TableBody>
+              {saleEmployeeSelected.map((sale) => (
+                <TableRow key={sale.id}>
+                  <TableCell>{dayjs(sale.date).format("DD/MM/YYYY")}</TableCell>
+                  <TableCell>{paymentMethods[sale.paymentMethod]}</TableCell>
+                  <TableCell>{formatCurrency(sale.value)}</TableCell>
+                  <TableCell>{formatCurrency(sale.commission)}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </Box>
       </Box>
+      <Button onClick={handlePrint} disabled={saleEmployeeSelected == ""}>
+        Imprimir
+      </Button>
     </Container>
   );
 };
